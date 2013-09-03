d402bd2386fa3ac4571f45629726ca396b397732
ToneD <t@syfen.com>
2013-09-04 08:44:23 +1000

    Initial push

---
+++ src/main/java/com/syfen/redis/Constants.java
@@ -0,0 +1,8 @@
+package com.syfen.redis;
+
+/**
+ * User: ToneD
+ * Created: 3/09/13 11:53 PM
+ */
+public class Constants {
+}
---
+++ src/main/java/com/syfen/redis/RedisCluster.java
@@ -0,0 +1,8 @@
+package com.syfen.redis;
+
+/**
+ * User: ToneD
+ * Created: 4/09/13 8:42 AM
+ */
+public class RedisCluster {
+}
