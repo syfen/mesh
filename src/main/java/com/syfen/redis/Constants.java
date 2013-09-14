6b8a8d424e8e412b959f54917903fa1e6d49b9da
ToneD <t@syfen.com>
2013-09-14 22:21:57 +1000

    Starting constants for zookeeper node structure

--- src/main/java/com/syfen/redis/Constants.java
+++ src/main/java/com/syfen/redis/Constants.java
@@ -5,4 +5,11 @@ package com.syfen.redis;
  * Created: 3/09/13 11:53 PM
  */
 public class Constants {
+
+    // zookeeper
+    public static final String ZK_REDIS_ROOT_NODE = "redis-cluster";
+    public static final String ZK_REDIS_CLIENTS_NODE = "clients";
+    public static final String ZK_REDIS_SERVERS_NODE = "servers";
+    public static final String ZK_REDIS_HOSTNAME_NODE = "hostname";
+    public static final String ZK_REDIS_PORT_NODE = "port";
 }
